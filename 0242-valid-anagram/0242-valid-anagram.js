6dd3029452ffec2829c5b8ea07cdfc1ab25c315d
Sohel Syed <83800834+syedsohel22@users.noreply.github.com>
2024-09-25 15:46:56 +0530

    Time: 75 ms (43.51%), Space: 52.9 MB (57.55%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+
+if(s.length!==t.length){
+    return false
+}
+
+    let obj = {}
+    for(let i=0;i<s.length;i++){
+        if(obj[s[i]]===undefined){
+            obj[s[i]] =1
+        }else{
+            obj[s[i]]++
+        }
+    }
+
+for(char of t){
+if(!obj[char]){
+    return false
+}else{
+    obj[char]--
+}
+}
+
+return true
+};
