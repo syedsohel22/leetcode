f72d99f699a31420760292e4a97d4ea582aa9ae7
Sohel Syed <83800834+syedsohel22@users.noreply.github.com>
2025-07-05 23:41:59 +0530

    Time: 21 ms (94.28%), Space: 66.9 MB (75.39%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    const anagramMap = new Map();
+  
+    
+    for (const str of strs) {
+        const key = str.split('').sort().join('');
+        if (!anagramMap.has(key)) {
+            anagramMap.set(key, []);
+        }
+        anagramMap.get(key).push(str);
+    }
+    return Array.from(anagramMap.values());
+};
