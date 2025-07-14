0490a668e40f67e9929c732711fb69a4032ad622
Sohel Syed <83800834+syedsohel22@users.noreply.github.com>
2025-07-14 23:17:16 +0530

    Time: 2 ms (82.45%), Space: 57.1 MB (47.84%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var largestNumber = function(nums) {
+    let strNums = nums.map((el)=> String(el))
+
+    strNums.sort((a,b) =>{
+        return (b+a)-  (a+b)
+    } )
+
+    if(strNums[0]==="0"){
+        return "0"
+    }
+
+    return strNums.join("")
+};
